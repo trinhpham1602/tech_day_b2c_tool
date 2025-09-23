1743b906c2684e1cfc4e3610c9834340e112448e
pdtrinh <phamdangtrinh1998@gmail.com>
2025-09-23 16:24:37 +0700

    init

--- src/app/page.js
+++ src/app/page.js
@@ -1,103 +1,120 @@
-import Image from "next/image";
+"use client";
+
+import { useState } from "react";
 
 export default function Home() {
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const [conversationData, setConversationData] = useState([]);
+  const [query, setQuery] = useState("");
+  const [loading, setLoading] = useState(false);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  const sendChatRequest = async () => {
+    setLoading(true);
+    const url = "http://127.0.0.1:8000/agent/chat";
+    const body = {
+      prompt: query,
+    };
+    try {
+      setQuery("");
+      setConversationData((prev) => [...prev, { who: "user", content: query }]);
+      const res = await fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(body),
+      });
+
+      if (!res.ok) {
+        const text = await res.text();
+        throw new Error(`HTTP ${res.status} - ${res.statusText}\n${text}`);
+      }
+      const data = await res.json();
+      setQuery("");
+      setConversationData((prev) => [
+        ...prev,
+        {
+          who: "agent",
+          content: JSON.stringify(data),
+        },
+      ]);
+    } catch (err) {
+      console.error("Request failed:", err);
+      throw err;
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div className="flex h-screen bg-gray-100">
+      {/* Sidebar */}
+      <aside className="w-1/4 bg-white border-r flex flex-col">
+        <div className="p-4 border-b font-bold text-lg flex justify-between">
+          <p>Chat</p>
+          <button onClick={() => setConversationData([])}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="size-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10"
+              />
+            </svg>
+          </button>
         </div>
+        <div className="flex-1 overflow-y-auto">Các cuộc hội thoại tại đây</div>
+      </aside>
+
+      {/* Chat Window */}
+      <main className="flex-1 flex flex-col">
+        {/* Messages */}
+        <div className="flex-1 p-4 overflow-y-auto space-y-2 flex flex-col">
+          {conversationData.map((item) => {
+            if (item.who !== "user") {
+              return (
+                <div
+                  key={Math.random().toString()}
+                  className="self-start bg-amber-50 p-2 rounded-lg w-2/3"
+                >
+                  {item.content}
+                </div>
+              );
+            }
+            return (
+              <div
+                key={Math.random().toString()}
+                className="self-end bg-green-300 p-2 rounded-lg w-2/3"
+              >
+                {item.content}
+              </div>
+            );
+          })}
+          {loading && <div>Đợi phản hồi...</div>}
+        </div>
+
+        {/* Input */}
+        <footer className="p-4 border-t bg-white flex gap-2">
+          <input
+            onChange={(event) => setQuery(event.target.value.trim())}
+            value={query}
+            type="text"
+            placeholder="Type a message..."
+            className="flex-1 rounded-lg px-3 py-2 focus:outline-none "
+          />
+          <button
+            onClick={sendChatRequest}
+            className="bg-blue-500 text-white px-4 py-2 rounded-lg"
+          >
+            Send
+          </button>
+        </footer>
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
